c31fed5032dd1ba4ac20fca90883c2cd0285dbf0
Loic Rouaud <loic.rouaud@gmail.com>
2023-07-24 00:42:39 +0200

    Lesson #5 if

---
+++ 1_python_basics_code/5_if/if_course.py
@@ -0,0 +1,16 @@
+# def even_number():
+#     n = int(input("Enter a number: "))
+#     if n % 2 == 0:
+#         print("Number is even")
+#     else:
+#         print("Number is odd")
+#
+#
+# if __name__ == '__main__':
+#     even_number()
+
+n = int(input("Enter a number: "))
+
+message = "Number is even" if n % 2 == 0 else "Number is odd"
+print(message)
+
